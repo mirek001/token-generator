e5a71bf8dc33bdbd4939134efa92699f05af95f3
Mirek Grzegorczyk <mirek.grzegorczyk@gmail.com>
2018-04-27 09:13:51 +0200

    0.9.1

--- src/token-generator.php
+++ src/token-generator.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace mirek001\TokenGenerator;
+//namespace mirek001\token_generator;
 
 
-class TokenGenerator{
+class token{
 	private $_secret_key = null;
 	private $_token = null;
+	public $_data = null;
 
 
-	public function __construct($data){
-		$this->_user_data = $userData;
+	public function __construct(){
+		$this->_data = md5('mirek001');
 	}
 
 	public function set_secret_key($data){
-		$data = md5($data.'wNeb3agAmr'..'c6u4Ejy4BGEcgXhiyp'..'VaV88Rh8mqvayntGctzP'..'aZFoT7dJ3nQaK9kZ'.$domain);
+		
 	}
 
 
